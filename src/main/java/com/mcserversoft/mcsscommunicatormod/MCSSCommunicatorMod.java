f1307fad12709c0aa3fe7a23ea7346366dc7633a
Robin Gabriël <robin@robingabriel.be>
2019-07-20 09:44:54 +0200

    10.0

--- src/main/java/com/mcserversoft/mcsscommunicatormod/MCSSCommunicatorMod.java
+++ src/main/java/com/mcserversoft/mcsscommunicatormod/MCSSCommunicatorMod.java
@@ -14,7 +14,7 @@ public class MCSSCommunicatorMod {
     public static final String MODID = "mcsscommunicatormod";
     public static final String NAME = "MCSSCommunicatorMod";
     public static final String WEBSITE = "https://www.mcserversoft.com";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "10.0";
 
     private Logger logger;
     private Config config;
